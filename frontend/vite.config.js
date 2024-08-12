20d087225727346d08ddd64d657b04cd8a5e9680
Chief <brianmwangi10808@gmail.com>
2024-08-12 09:50:18 +0300

    deployment of frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // This ensures that the build files are output to the 'dist' directory
+    outDir: '.build', // Change the output directory to '.build'
   },
 })
