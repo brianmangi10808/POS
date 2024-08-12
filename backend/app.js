0592e9764fc61da97b75ae5dbafb3c559fe2d2a9
Chief <brianmwangi10808@gmail.com>
2024-08-12 10:02:33 +0300

    backend deployment

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 3000;
+const port = 3306;
 const bcrypt = require('bcryptjs');
 const mysql = require('mysql');
 
