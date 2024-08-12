c6dc59a7103c68b07cb6a4e3f042b77d05ab4d20
Chief <brianmwangi10808@gmail.com>
2024-08-12 10:11:01 +0300

    second backend deployment

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 3306;
+const port = 3000;
 const bcrypt = require('bcryptjs');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 // Middleware for parsing JSON data
 app.use(express.json());
