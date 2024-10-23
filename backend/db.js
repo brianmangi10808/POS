42a818789f29d382ba9831fa3e79d88431faf644
Chief <brianmwangi10808@gmail.com>
2024-10-23 22:25:21 +0300

    remove env

--- backend/db.js
+++ backend/db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 
 // Create a MySQL connection
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: '102.130.118.213',
+    user: 'render_user',
+    password: 'root',
+    database: 'pos_data'
 });
 
 // Connect to MySQL
