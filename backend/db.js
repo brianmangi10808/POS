3a3a9b37d97aeb558475d49464f523b3d4cdf17e
Chief <brianmwangi10808@gmail.com>
2024-10-23 14:38:07 +0300

    data

--- backend/db.js
+++ backend/db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 
 // Create a MySQL connection
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'pos_data'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 // Connect to MySQL
